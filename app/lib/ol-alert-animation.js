cba5d039470b3d7db7aa1334cd1c22a37cb9c44b
Andreas Fachathaler <faandi@fachi.net>
2023-06-07 14:53:14 +0200

    feat: thicker alert animation circle stroke

--- app/lib/ol-alert-animation.js
+++ app/lib/ol-alert-animation.js
@@ -28,7 +28,7 @@ function flash(layer, feature) {
         radius: radius,
         stroke: new Stroke({
           color: 'rgba(255, 0, 0, ' + opacity + ')',
-          width: 0.25 + opacity,
+          width: 8 + opacity,
         }),
       }),
     });
